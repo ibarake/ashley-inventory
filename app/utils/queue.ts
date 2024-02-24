407996193c9b9853e2032f86d9b71aad9db9e19e
unknown <issabarake@gmail.com>
2024-02-23 23:44:21 -0500

    fixing backend status not uploading all 14k rows

--- app/utils/queue.ts
+++ app/utils/queue.ts
@@ -1,16 +1,10 @@
 import Bull from 'bull';
 import { processCSVJob } from './process-csv-job'; // You will create this function next
 
-// Configure your Redis connection details
-const redisConfig = {
-  host: '127.0.0.1',
-  port: 6379,
-  // if you use Redis with password
-  // password: 'your-redis-password',
-};
-
-// Create a new Bull queue for CSV processing
-export const csvQueue = new Bull('csv-processing', { redis: redisConfig });
+// Create a new Bull queue for CSV processing using a Redis URL
+export const csvQueue = new Bull('csv-processing', {
+    redis: process.env.REDIS_URL,
+  });
 
 // Process jobs in concurrency, adjust '5' to your needs
 csvQueue.process(5, async (job) => {
