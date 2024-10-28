8b305feed101ca9ca7ccfd19cef6ce469c5e8dba
julianfalquez <jfalquezr@gmail.com>
2024-10-27 19:03:37 -0500

    added logs

--- app/utils/process-csv-job.ts
+++ app/utils/process-csv-job.ts
@@ -6,14 +6,16 @@ import { parseCSVFromFile } from './parse-csv';
 export async function processCSVJob(job: Job<any>) {
   const { filepath, parse } = job.data;
   
-  console.log('Processing CSV file:', filepath);
+  console.log('Processing CSV file:', filepath, "proccessing type :" , parse);
 
   try {
     // Assuming parseCSVFromFileStatus is an async function
     if (parse === 'status') {
+      console.log("entro status")
       await parseCSVFromFileStatus(filepath);
     }
     if (parse === 'inv') {
+      console.log("entro inventory")
       await parseCSVFromFile(filepath);
     }
     return { success: true, filepath };
